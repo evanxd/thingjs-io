d6b5e8329138ea779aa0e8ba802eaa3c7c70467c
Evan Tseng <evan@tseng.io>
2016-07-31 22:51:12 +0800

    Implement UART

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var util = require('util');
 var GPIO = require('gpio-js');
+var UART = require('./lib/uart');
 
 function IO (pins, mode) {
   var instance;
@@ -9,6 +10,10 @@ function IO (pins, mode) {
     case 'in':
     case 'out':
       instance = new GPIO(pins, mode);
+      break;
+    case 'uart':
+      instance = new UART(pins);
+      break;
   }
   return instance;
 }
