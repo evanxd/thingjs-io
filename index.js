06cec2ee78a80fe43d8799f50fd6f792be155a12
Evan Tseng <evan@tseng.io>
2016-08-13 12:37:42 +0800

    Update GPIO factory

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ var GPIO = require('./lib/gpio');
 var PWM  = require('./lib/pwm'); 
 var UART = require('./lib/uart');
 
-function IO (io, type, mode) {
+function IO (io, type, options) {
+  options = options || {};
   var IO;
   switch (type) {
     case 'gpio':
-      IO = new GPIO(io, mode);
+      options.mode = options.mode || 'out';
+      IO = new GPIO(io, options.mode);
       break;
     case 'pwm':
       IO = new PWM(io);
