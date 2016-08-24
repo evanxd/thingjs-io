d5792ee03cc1a4d23db77935a3dad7ff46b68841
Evan Tseng <evan@tseng.io>
2016-08-24 11:24:28 +0800

    Remap the pin numbers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var exec = require('child_process').execSync;
+var linkit7688 = require('iotjs-linkit7688');
 var util = require('util');
 var GPIO = require('./lib/gpio');
 var PWM  = require('./lib/pwm'); 
@@ -7,6 +9,15 @@ var UART = require('./lib/uart');
 
 function IO (io, type, options) {
   var IO;
+
+  // Remap the `io` pin, from board's pin number to system's pin number.
+  var pin = linkit7688['p' + io];
+  io = typeof pin[type] === 'number' ? pin[type] : pin[type].index;
+
+  // FIXME: Hard code the `mt7688_pinmux` command. We should use a cross-platform solution.
+  // Use pinmux to switch the pin to the right mode.
+  exec('mt7688_pinmux set ' + pin.group + ' ' + type);
+
   switch (type) {
     case 'gpio':
       IO = new GPIO(io, options);
